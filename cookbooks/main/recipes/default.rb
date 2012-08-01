282e5345d2268bd40120678299754091da21bd52
Joshua Pinter <joshuapinter@gmail.com>
2012-08-01 13:31:38 -0600

    Added database requirement in main recipe.

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,6 +4,8 @@
 #  }
 #end
 
+require_recipe 'database'
+
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 require_recipe "sphinx"
 
